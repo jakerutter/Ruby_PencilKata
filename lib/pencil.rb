617201478bd3a72463c6942a95a1127409040bb1
Jacob Rutter <rutter.jacob@gmail.com>
2019-01-30 16:40:41 -0500

    updated pencil class

--- lib/pencil.rb
+++ lib/pencil.rb
@@ -1,65 +1,43 @@
 class Pencil
-  attr_accessor :length, :durability
 
-  def initialize(length=nil, durability=nil)
+  def initialize(length, durability)
     @length = length
     @durability = durability
   end
 
-# Does this really give it a default length though?  This method seems superfluous
-  def default_length(length)
-    length = length
-    length
-  end
-
-#Same as above
-  def default_durability(durability)
-    durability = durability
-    durability
-  end
-
-# Same as above
-  def eraser_durability(eraser_durability)
-    eraser_durability = eraser_durability
-    eraser_durability
-  end
+  attr_accessor :length, :durability
 
 #All writes should go through this method
   def write(text, paper)
+    durability = @durability
+    if text.length <= durability
+      @durability -= text.length
+
+    else
+      text = text.slice(0...durability) + " "*(text.length-durability)
+      @durability = 0
+    end
     paper = text
     paper
   end
 
-  def erase(phrase, erase_word)
-    index = phrase.rindex(erase_word)
-    a, b = phrase.slice!(index, erase_word.length), phrase
+  def erase(paper, erase_word)
+    index = paper.rindex(erase_word)
+    a, b = paper.slice!(index, erase_word.length), paper
     b = b.gsub(/\s+/, ' ')
-    phrase = b
-    phrase
+    paper = b
+    paper
   end
 
-  def sharpen(length, default_durability, current_durability)
+  def sharpen(current_durability)
+    length = @length
+    durability = @durability
     if length > 0
-      durability = default_durability
       length -= 1
     else
       durability = current_durability
   end
     return length, durability
 end
-
-#Should be able to refactor this method to be write(paper,text) and use @durability as a property instead of passing it in
-  def write_text_reduce_pencil_durability(durability, text)
-    if text.length <= durability
-      write(text, "")
-      durability -= text.length
-
-    else
-      text = text.slice(0...durability)
-      write(text, "")
-      durability = 0
-    end
-      durability
-  end
 
 end
